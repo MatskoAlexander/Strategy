d5b5c7095e90e9f395e1eb84885af9c70b97bc35
MatskoAlexander <AMatsko54@mail.ru>
2019-02-15 15:57:00 +0700

    start

---
+++ main.py
@@ -0,0 +1,45 @@
+#TODO: переменные
+POPULATION = 2000
+MONEY = 70000
+SEEDS = 10000
+DISTEMPER = 0.02
+TERRITORY = 30
+ARMY = 500
+ARMY_DISTEMPER = 0.00
+MORTALITY = 20
+FERTILITY = 20
+MONTH = 1
+YEAR = 1
+
+#TODO: functions
+def sale_seeds():
+    pass
+
+def buy_seeds():
+    pass
+
+def sowing_seeds():
+    pass
+
+def distribution_seeds():
+    pass
+
+def start_war():
+    pass
+
+def conquest_territory():
+    pass
+
+def enlarge_army():
+    pass
+
+def taxes():
+    pass
+#TODO: ситуация
+
+#TODO: вызов функций
+
+#TODO: считывание
+
+#TODO: операции
+
