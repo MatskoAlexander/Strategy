ebee58bd899e18439fb354d137c2f610bebf6859
MatskoAlexander <AMatsko54@mail.ru>
2019-02-15 16:46:34 +0700

    start

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-#TODO: переменные
+# TODO: переменные
+
 POPULATION = 2000
 MONEY = 70000
 SEEDS = 10000
@@ -11,35 +12,45 @@ FERTILITY = 20
 MONTH = 1
 YEAR = 1
 
-#TODO: functions
+# TODO: functions
+
+
 def sale_seeds():
     pass
+
 
 def buy_seeds():
     pass
 
+
 def sowing_seeds():
     pass
+
 
 def distribution_seeds():
     pass
 
+
 def start_war():
     pass
+
 
 def conquest_territory():
     pass
 
+
 def enlarge_army():
     pass
 
+
 def taxes():
     pass
-#TODO: ситуация
 
-#TODO: вызов функций
 
-#TODO: считывание
+# TODO: ситуация
 
-#TODO: операции
+# TODO: вызов функций
 
+# TODO: считывание
+
+# TODO: операции
